06d5e36d89eae69aa0998f1d86c9c57d7915fd0d
thetomtoro <thetomtoro@gmail.com>
2026-03-15 00:07:43 -0500

    fix: fix Tailwind v3 config, externalize chromadb native deps for build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: [
+      "chromadb",
+      "@chroma-core/default-embed",
+      "chromadb-default-embed",
+      "@huggingface/transformers",
+      "onnxruntime-node",
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push({
+        "onnxruntime-node": "commonjs onnxruntime-node",
+        "@huggingface/transformers": "commonjs @huggingface/transformers",
+      });
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
